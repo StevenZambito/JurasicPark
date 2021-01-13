d6d6170ab91ed571d54d70fff8c2588c349e41fe
StevenZambito <zambito.steven@icloud.com>
2021-01-13 12:58:49 -0500

    Created banner method and Dino class

--- Program.cs
+++ Program.cs
@@ -2,11 +2,33 @@
 
 namespace JurasicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Dino
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public string WhenAcquired { get; set; }
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+
+
     }
-  }
+
+
+    class Program
+    {
+        static void BannerMessage(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            BannerMessage("Welcome to Jurassic Park!");
+
+            BannerMessage("Goodbye!");
+        }
+    }
 }
