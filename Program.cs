7e84e5a9c6542d88a6ca52ecbda96cd1078c47a8
StevenZambito <zambito.steven@icloud.com>
2021-01-13 13:33:01 -0500

    Created instances of Dino class and created menu options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JurasicPark
 {
@@ -6,7 +7,7 @@ namespace JurasicPark
     {
         public string Name { get; set; }
         public string DietType { get; set; }
-        public string WhenAcquired { get; set; }
+        public DateTime WhenAcquired { get; set; }
         public int Weight { get; set; }
         public int EnclosureNumber { get; set; }
 
@@ -26,7 +27,47 @@ namespace JurasicPark
         }
         static void Main(string[] args)
         {
+
+            var dinosaurs = new List<Dino>() {
+            new Dino()
+            {
+              Name = "Rex",
+              DietType = "Carnivore",
+              WhenAcquired = DateTime.Now,
+              Weight = 15000,
+              EnclosureNumber = 1,
+            },
+            new Dino()
+            {
+              Name = "Camara",
+              DietType = "Herbivore",
+              WhenAcquired = DateTime.Now,
+              Weight = 44000,
+              EnclosureNumber = 2,
+            },
+            new Dino()
+            {
+              Name = "Brack",
+              DietType = "Herbivore",
+              WhenAcquired = DateTime.Now,
+              Weight = 75000,
+              EnclosureNumber = 3,
+            },
+
+          };
+
             BannerMessage("Welcome to Jurassic Park!");
+
+            Console.WriteLine();
+            Console.WriteLine("Menu:");
+            Console.WriteLine();
+            Console.WriteLine("View");
+            Console.WriteLine("Add");
+            Console.WriteLine("Remove");
+            Console.WriteLine("Transfer");
+            Console.WriteLine("Summary");
+            Console.WriteLine("Quit");
+            Console.WriteLine();
 
             BannerMessage("Goodbye!");
         }
